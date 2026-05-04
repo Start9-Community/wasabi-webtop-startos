68265562a66c6cd6ee53360063069a358e336327
Remco Ros <remcoros@live.nl>
2026-05-04 17:23:20 +0200

    chore: bump sdk to 1.4.1, version to 2.7.2:1-beta.3

--- startos/versions/v2.7.2.ts
+++ startos/versions/v2.7.2.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const v2_7_2 = VersionInfo.of({
-  version: '2.7.2:1-beta.2',
+  version: '2.7.2:1-beta.3',
   releaseNotes: {
     en_US: 'Revamped for StartOS 0.4',
   },
