0def18c168a3308585a7bf023bd0472f9864ae7f
Remco Ros <remcoros@live.nl>
2026-07-24 18:36:45 +0200

    update Wasabi to 2.8.1

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -2,10 +2,11 @@ import { VersionGraph } from '@start9labs/start-sdk'
 import { current, WASABI_VERSION } from './current'
 import { v2_7_2 } from './v2.7.2'
 import { v2_7_2_2 } from './v2.7.2_2'
+import { v2_7_2_3 } from './v2.7.2_3'
 
 export const versionGraph = VersionGraph.of({
   current,
-  other: [v2_7_2_2, v2_7_2],
+  other: [v2_7_2_3, v2_7_2_2, v2_7_2],
 })
 
 export { WASABI_VERSION }
